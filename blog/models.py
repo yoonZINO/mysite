dc6efa9278359c5a43d47dbbb39f1107b47cc2e1
yoonZINO <yoonzino449@gmail.com>
2026-09-06 01:54:12 +0330

    database
    create diagram usinf drawsql and create the diagram on project and check that usin the sql browser

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    #image
+    #tag
+    #category
+    #author
+    counted_view = models.IntegerField(default=0) #deafult is 0
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,12 @@ from django_app.views import Home_view, About_view, Contact_view
 app_name = 'blog' #assign app name to detemine for buttons in site when click them and move to baid page
 urlpatterns = [
     path('',views.blog_view, name='index'), #for blog/index
+    path('blog-home.html/', views.blog_view, name='indexblog'),
     path('index.html/', Home_view, name='index_blog'),
     path('about.html/', About_view, name='about_blog'),
     path('contact.html/', Contact_view, name='contact_blog'),
-    path('single/', views.blog_single, name='single'), #for blog/single
-    path('single/index.html/', Home_view, name='index_single_blog'),
-    path('single/about.html/', About_view, name='about_single_blog'),
-    path('/single/contact.html/', Contact_view, name='contact_single_blog'),
+    path('blog-single/', views.blog_single, name='single'), #for blog/single
+    path('blog-single/index.html/', Home_view, name='index_single_blog'),
+    path('blog-single/about.html/', About_view, name='about_single_blog'),
+    path('/blog-single/contact.html/', Contact_view, name='contact_single_blog'),
 ]
