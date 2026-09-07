c0fad9a6dae10922321f571ad2e90e8789b0a2cd
yoonZINO <yoonzino449@gmail.com>
2026-09-08 00:32:03 +0330

    filter by date
    filtering post by created_date and creating p4 ibject with the title forth post

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
+class Postadmin(admin.ModelAdmin): #name of class-> [name of model][changing admin]
+     date_hierarchy ='created_date'
 
-admin.site.register(Post) #registering model to be shown in admin page
+admin.site.register(Post, Postadmin) #registering model to be shown in admin page
